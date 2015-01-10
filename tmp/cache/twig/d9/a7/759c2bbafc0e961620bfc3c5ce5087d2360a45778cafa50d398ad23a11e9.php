1980430760b9e2ac8f028c743409b79edb5c0035
Joost Lubach <joost@yoazt.com>
2015-01-10 15:20:43 +0100

    Initial commit

---
+++ tmp/cache/twig/d9/a7/759c2bbafc0e961620bfc3c5ce5087d2360a45778cafa50d398ad23a11e9.php
@@ -0,0 +1,53 @@
+<?php
+
+/* /index.html.twig */
+class __TwigTemplate_d9a7759c2bbafc0e961620bfc3c5ce5087d2360a45778cafa50d398ad23a11e9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!doctype html>
+
+<html lang=\"nl\">
+
+  <head>
+    <meta charset=\"utf-8\">
+
+    <title>Grand Tour</title>
+    <meta name=\"author\" content=\"Joost Lubach\" />
+    <meta name=\"description\" content=\"Grand Tour - Ontdek Zwitserland\" />
+
+    <link rel=\"stylesheet\" href=\"css/all.css?v1.0\" />
+
+    <!--[if lt IE 9]>
+    <script src=\"http://html5shiv.googlecode.com/svn/trunk/html5.js\"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    Hoi
+  </body>
+
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "/index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+
+define('ROOT_PATH', dirname(__FILE__) . '/..');
+
+require_once ROOT_PATH . '/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem(ROOT_PATH . '/twig');
+
+$twig = new Twig_Environment($loader);
+
+$path = preg_replace('|^(?:/.*?\.php)(.*)$|', '$1', $_SERVER['REQUEST_URI']);
+
+try {
+  echo $twig->render("$path.twig");
+} catch (Twig_Error_Loader $ex) {
+  http_response_code(404);
+  echo "$path not found";
+}
+
+?>
